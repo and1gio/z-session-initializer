2608a88fae67f2798266d327fb8e2b9aec55a1e6
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2018-05-08 16:32:36 +0400

    changed custom store with mongo-connect

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # z-session-initializer
+
+### config/z-session.js
+```js
+exports.default = function (app) {
+    return {
+        zSession: {
+            secret: 'TheAppSecret',
+            resave: true,
+            saveUninitialized: false,
+            store: {
+                url: 'mongodb://192.168.1.183:27017/msda-sessions-store-development',
+                ttl: 14 * 24 * 3600, // = 14 days. Default
+                stringify: false
+            }
+        }
+    }
+};
+```
--- z-session-initializer.js
+++ z-session-initializer.js
@@ -1,43 +1,16 @@
 'use strict';
 
 module.exports = {
-    run: function (app, next) {
-        var session = require('express-session');
-        var ZSessionServerClient = require('z-session-server-client')(session);
+    run: (app, next) => {
+        const session = require('express-session');
+        const MongoStore = require('connect-mongo')(session);
 
-        app.zSessionServerClient = new ZSessionServerClient({
-            host: app.config.zSessionServerClient.host,
-            port: app.config.zSessionServerClient.port,
-            path: app.config.zSessionServerClient.path
-        });
-        
-        var sessionConfig = {
-            store: app.zSessionServerClient
-        };
-
-        app.zSessionManager = ZSessionServerClient.zSessionManager;
-
-        if(app.config.zSessionServerClient.secret !== undefined){
-            sessionConfig.secret = app.config.zSessionServerClient.secret;
-        }
-
-        if(app.config.zSessionServerClient.resave !== undefined){
-            sessionConfig.resave = app.config.zSessionServerClient.resave;
-        }
-
-        if(app.config.zSessionServerClient.saveUninitialized !== undefined){
-            sessionConfig.saveUninitialized = app.config.zSessionServerClient.saveUninitialized;
-        }
-
-        if(app.config.zSessionServerClient.cookie !== undefined){
-            sessionConfig.cookie = app.config.zSessionServerClient.cookie;
-        }
-
-        if(app.config.zSessionServerClient.domain !== undefined){
-            sessionConfig.domain = app.config.zSessionServerClient.domain;
-        }
-
-        app.express.use(session(sessionConfig));
+        app.express.use(session({
+            resave: zSession.resave,
+            saveUninitialized: zSession.saveUninitialized,
+            secret: app.config.zSession.secret,
+            store: new MongoStore(app.config.zSession.store)
+        }));
 
         next();
     }
